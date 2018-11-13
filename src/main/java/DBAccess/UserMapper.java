d9a37e8acf8bba6d358c3a31f68e6584455caf94
Jonatan Hjelm <cph-jh409@cphbusiness.dk>
2018-11-13 10:48:53 +0100

    login virker nu

--- src/main/java/DBAccess/UserMapper.java
+++ src/main/java/DBAccess/UserMapper.java
@@ -37,15 +37,15 @@ public class UserMapper {
     public static User login(String email, String password) throws LoginSampleException {
         try {
             Connection con = Connector.connection();
-            String SQL = "SELECT id, role FROM User "
+            String SQL = "SELECT UserID, role FROM User "
                     + "WHERE email=? AND password=?";
             PreparedStatement ps = con.prepareStatement(SQL);
             ps.setString(1, email);
             ps.setString(2, password);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
-                String role = rs.getString("role");
-                int id = rs.getInt("id");
+                String role = rs.getString("role").toUpperCase();
+                int id = rs.getInt("UserID");
                 User user = new User(email, password, Role.valueOf(role));
                 user.setId(id);
                 return user;
@@ -72,16 +72,15 @@ public class UserMapper {
                 int id = rs.getInt("id");
                 User user = new User(email, password, Role.valueOf(role));
                 user.setId(id);
-                if (email.equals(userEmail) && password.equals(userPW)) {
-                    return user;
-                }
+                return user;
+
             } else {
                 throw new LoginSampleException("Could not validate user");
             }
         } catch (ClassNotFoundException | SQLException ex) {
             throw new LoginSampleException(ex.getMessage());
         }
-        return null;
+
     }
 
 }
--- src/main/java/PresentationLayer/Login.java
+++ src/main/java/PresentationLayer/Login.java
@@ -16,23 +16,20 @@ public class Login extends Command {
 
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
-                String email = request.getParameter("email");
+        String email = request.getParameter("email");
         String password = request.getParameter("password");
         User user;
-        try {
-            user = LogicFacade.getUser(email, password);
-            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
-                User user1 = LogicFacade.login(user.getEmail(), user.getPassword());
-                HttpSession session = request.getSession(true);
-                session.setAttribute("user", user1);
-                session.setAttribute("role", user1.getRole());
-                request.setAttribute("menu", gen.generateMenu(request));
+//        try {
+        user = LogicFacade.login(email, password);
+        HttpSession session = request.getSession(true);
+        session.setAttribute("user", user);
+        session.setAttribute("role", user.getRole());
+        request.setAttribute("menu", gen.generateMenu(request));
 
-            }
-        } catch (Exception ex) {
-            request.setAttribute("error", "Wrong username/password");
-            ex.printStackTrace();
-        }
+//        } catch (Exception ex) {
+////            request.setAttribute("error", "Wrong username/password");
+//            ex.printStackTrace();
+//        }
         return "index";
     }
 
