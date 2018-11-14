8b4f4f0adc256c3296bbdd85b33ba7ba485710a8
mf226 <cph-mf226@cphbusiness.dk>
2018-11-14 10:22:14 +0100

    Added method for DBmapper to materials. Logicfacade adjustments

--- src/main/java/FunctionLayer/LogicFacade.java
+++ src/main/java/FunctionLayer/LogicFacade.java
@@ -1,32 +1,28 @@
 package FunctionLayer;
 
+import DBAccess.MaterialAndOrderMapper;
 import DBAccess.UserMapper;
+import java.util.List;
 
 /**
  * The purpose of LogicFacade is to...
+ *
  * @author kasper
  */
 public class LogicFacade {
 
-    public static User login( String email, String password ) throws LoginSampleException {
-        return UserMapper.login( email, password );
-    } 
+    public static User login(String email, String password) throws LoginSampleException {
+        return UserMapper.login(email, password);
+    }
 
-    public static User createUser( String email, String password, Role role ) throws LoginSampleException {
-        User user = new User(email, password, role );
-        UserMapper.createUserCustomer( user );
+    public static User createUser(String email, String password, Role role) throws LoginSampleException {
+        User user = new User(email, password, role);
+        UserMapper.createUser(user);
         return user;
     }
-    
-    /**
-     *
-     * @param email
-     * @param password
-     * @return
-     * @throws Exception
-     */
-    public static User getUser(String email, String password) throws LoginSampleException {
-        return UserMapper.getUser(email, password);
+
+    public static List<Material> getAllMaterials() throws LoginSampleException {
+        return MaterialAndOrderMapper.getAllMaterials();
     }
 
 }
