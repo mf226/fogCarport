cacebe164446a629d60af9286ef900542778af12
mf226 <cph-mf226@cphbusiness.dk>
2018-11-14 12:30:22 +0100

    adjusted Material, added unfinished Order.

---
+++ src/main/java/FunctionLayer/Order.java
@@ -0,0 +1,15 @@
+package FunctionLayer;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Fen
+ */
+public class Order {
+    private int length;
+    private int width;
+    private int height;
+    private String description;
+    private ArrayList<Material> materials;
+}
