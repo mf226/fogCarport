13d1f49c2b08cea4d3c8c3bfdbbd62aeca7b6dc2
mf226 <cph-mf226@cphbusiness.dk>
2018-11-14 13:02:45 +0100

    createCarport method, untested

--- src/main/java/DBAccess/MaterialAndOrderMapper.java
+++ src/main/java/DBAccess/MaterialAndOrderMapper.java
@@ -16,14 +16,14 @@ import java.util.List;
  * @author Fen
  */
 public class MaterialAndOrderMapper {
-    
+
     public static List<Material> getAllMaterials() throws LoginSampleException {
         try {
             ArrayList<Material> materials = new ArrayList();
             Connection con = Connector.connection();
             String SQL = "SELECT itemNumber, materialName, unit, price FROM FogDB.Materials;";
             PreparedStatement ps = con.prepareStatement(SQL);
-            
+
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 int itemNumber = rs.getInt("itemNumber");
@@ -37,5 +37,28 @@ public class MaterialAndOrderMapper {
             throw new LoginSampleException(ex.getMessage());
         }
     }
-    
+
+    public static Material getMaterial(int itemNumber) throws LoginSampleException {
+        try {
+
+            Connection con = Connector.connection();
+            String SQL = "SELECT materialName, unit, price FROM FogDB.Materials"
+                    + "WHERE itemNumber=?";
+            PreparedStatement ps = con.prepareStatement(SQL);
+            ps.setInt(1, itemNumber);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                String materialName = rs.getString("materialName");
+                String unit = rs.getString("unit");
+                double price = rs.getDouble("price");
+                return new Material(itemNumber, materialName, unit, price);
+            } else {
+                throw new LoginSampleException("Material doesn't exist.");
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            throw new LoginSampleException(ex.getMessage());
+        }
+    }
+
 }
