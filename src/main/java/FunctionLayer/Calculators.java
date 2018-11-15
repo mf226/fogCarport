ccd1981fb04906a68b2b96a4cc64ef4a73ea7c00
Porselicious <porseg69@gmail.com>
2018-11-15 10:27:08 +0100

    Added DB script to initializer

--- src/main/java/FunctionLayer/Calculators.java
+++ src/main/java/FunctionLayer/Calculators.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package FunctionLayer;
 
 /**
@@ -13,15 +8,15 @@ public class Calculators {
 
     public static int postsCalc(int length, int width) {
         int posts = length / 100 + 1;
-        if (width/2 >= 5) {
+        if (width / 2 >= 5) {
             posts++;
         }
         return posts * 2;
     }
-    
-    public static int rafterCalc(int length){
+
+    public static int rafterCalc(int length) {
         int rafter = length / 50 + 1;
-        
+
         return rafter;
     }
 }
