9ed57e161592ea4b113ac4ea655dd8a173b19c76
Porselicious <porseg69@gmail.com>
2018-11-26 12:27:16 +0100

    updated DBinit

--- src/main/java/DBAccess/dbInit.sql
+++ src/main/java/DBAccess/dbInit.sql
@@ -5,14 +5,30 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema FogDB
 -- -----------------------------------------------------
-DROP DATABASE IF EXISTS `FogDB` ;
+DROP SCHEMA IF EXISTS `FogDB` ;
 
 -- -----------------------------------------------------
 -- Schema FogDB
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `FogDB` DEFAULT CHARACTER SET latin1 ;
+USE `FogDB` ;
 
----------------------------------------------------
+-- -----------------------------------------------------
+-- Table `FogDB`.`Materials`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `FogDB`.`Materials` (
+  `itemNumber` INT(11) NOT NULL AUTO_INCREMENT,
+  `materialName` VARCHAR(45) NOT NULL,
+  `unit` VARCHAR(45) NOT NULL,
+  `price` DOUBLE NULL DEFAULT NULL,
+  `category` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`itemNumber`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
 -- Table `FogDB`.`User`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `FogDB`.`User` (
@@ -27,36 +43,26 @@ ENGINE = InnoDB
 AUTO_INCREMENT = 5
 DEFAULT CHARACTER SET = latin1;
 
-
 -- -----------------------------------------------------
 -- Table `FogDB`.`Order`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `FogDB`.`Order` (
   `orderID` INT(11) NOT NULL AUTO_INCREMENT,
-  `orderDescription` VARCHAR(45) NOT NULL,
-  `price` DOUBLE NULL DEFAULT NULL,
-  `User_UserID` INT(11) NOT NULL,
+  `userID` INT(11) NOT NULL,
+  `length` INT(11) NULL,
+  `width` INT(11) NULL,
+  `height` INT(11) NULL,
+  `angle` INT(11) NULL,
+  `finalizedPrice` DOUBLE NULL DEFAULT NULL,
   `orderDate` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`orderID`, `User_UserID`),
-  INDEX `fk_Order_User_idx` (`User_UserID` ASC),
+  `status` ENUM('pending', 'approved', 'shipped') NULL,
+  PRIMARY KEY (`orderID`, `userID`),
+  INDEX `fk_Order_User_idx` (`userID` ASC),
   CONSTRAINT `fk_Order_User`
-    FOREIGN KEY (`User_UserID`)
+    FOREIGN KEY (`userID`)
     REFERENCES `FogDB`.`User` (`UserID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `FogDB`.`Træ_og_Tagplader`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FogDB`.`Materials` (
-  `itemNumber` INT NOT NULL AUTO_INCREMENT,
-  `materialName` VARCHAR(45) NOT NULL,
-  `unit` VARCHAR(45) NOT NULL,
-  `price` DOUBLE NULL DEFAULT NULL,
-  PRIMARY KEY (`itemNumber`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
@@ -70,23 +76,31 @@ CREATE TABLE IF NOT EXISTS `FogDB`.`OrderDetails` (
   `itemNumber` INT(11) NOT NULL,
   PRIMARY KEY (`orderID`, `itemNumber`),
   INDEX `fk_OrderDetails_Materials_idx` (`itemNumber` ASC),
-  CONSTRAINT `fk_OrderDetails_Order`
-    FOREIGN KEY (`orderID`)
-    REFERENCES `FogDB`.`Order` (`orderID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_OrderDetails_Materials`
     FOREIGN KEY (`itemNumber`)
     REFERENCES `FogDB`.`Materials` (`itemNumber`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_OrderDetails_Order`
+    FOREIGN KEY (`orderID`)
+    REFERENCES `FogDB`.`Order` (`orderID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
-USE `FogDB` ;
-
-INSERT INTO `FogDB`.`Materials` (`itemNumber`, `materialName`, `unit`, `price`) VALUES ('1', '45x195', 'm', '100');
-INSERT INTO `FogDB`.`Materials` (`itemNumber`, `materialName`, `unit`, `price`) VALUES ('2', '97x97', 'm', '90');
-INSERT INTO `FogDB`.`User` (`UserID`, `email`, `password`, `role`) VALUES ('1', 'testuser@test.com', 'test', 'Customer');
-
+INSERT INTO `FogDB`.`Materials` (`itemNumber`, `materialName`, `unit`, `price`, `category`) VALUES ('1', '45x195', 'm', '100', 'Træ'),
+('2', '97x97', 'm', '90', 'Træ'),
+('3', '4,5x60 mm skruer á 200 stk', 'stk', '300', 'Skruer'), 
+('4', 'Vinkelbeslag 35', 'stk', '35', 'Beslag'),
+('5', 'Plastmo	Ecolite	blåtonet', 'kvm', '100', 'Tag'),
+('6', 'Røde tegl', 'stk', '25', 'Tagbelægning'),
+('7', '25x150', 'm', '60', 'Beklædning'),
+('8', 'Cement 10 kg', 'kg', '80', 'Andet'),
+('9', 'Sort glaserede tegl', 'stk', '55', 'Tagbeklædning'),
+('10', 'Plastmo	Ecolite	klar', 'stk', '52', 'Tag');
+INSERT INTO `FogDB`.`User` (`UserID`, `email`, `password`, `role`) VALUES ('1', 'testuser@test.com', 'test', 'Customer'),
+('2', 'admin', 'admin', 'ADMIN');
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
