1c3f4833ae25f8e0ae69f7132e2a1955b7803b12
mf226 <cph-mf226@cphbusiness.dk>
2018-11-30 12:31:02 +0100

    Fixed rooftype bug

--- src/main/java/FunctionLayer/Calculators.java
+++ src/main/java/FunctionLayer/Calculators.java
@@ -93,14 +93,14 @@ public class Calculators {
 
     public static double calcRoof(Order order) {
         if (order.getAngle() == 0) {
-            double squaremeters =((order.getLength() + RulesAndConstants.ROOF_LENGTH_EXTRA) / 100)
-                    * ((order.getHeight() + RulesAndConstants.ROOF_WIDTH_EXTRA) / 100);
-            return squaremeters;
+            double squaremeters =(order.getCarportWoodMaterials().get(RulesAndConstants.CARPORT_REM_DESCRIPTION).getCmLengthEach() / 100)
+                    * (order.getCarportWoodMaterials().get(RulesAndConstants.CARPORT_RAFTER_FLATROOF_DESCRIPTON).getCmLengthEach() / 100);
+            return (int) squaremeters + 1;
         }
         else {
             double squaremeters = (order.getCarportWoodMaterials().get(RulesAndConstants.CARPORT_REM_DESCRIPTION).getCmLengthEach() / 100)
                     * (order.getCarportWoodMaterials().get(RulesAndConstants.CARPORT_RAFTER_ANGLEDROOF_SIDE_DESCRIPTION).getCmLengthEach() / 100);
-            return squaremeters * RulesAndConstants.TILES_PER_SQUAREMETER;
+            return (int) (squaremeters * RulesAndConstants.TILES_PER_SQUAREMETER) + 1;
                     
         }
     }
